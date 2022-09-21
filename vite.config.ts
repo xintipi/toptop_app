f37cbe267fc5ba9499a054d550f82e7b504f46c3
Nguyen Huu Trung <huutrung.mmt@gmail.com>
2022-09-21 11:00:44 +0700

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,70 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dayjs from 'dayjs';
+// @ts-ignore
+import pkg from './package.json';
+// @ts-ignore
+import * as path from 'path';
+
+const { dependencies, devDependencies, name, version, author } = pkg;
+const __APP_INFO__ = {
+  pkg: { dependencies, devDependencies, name, version, author },
+  lastBuildTime: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+};
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/',
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src'),
+      },
+    ],
+  },
+  server: {
+    host: true,
+    port: 8080,
+    open: true,
+  },
+  preview: {
+    port: 5555,
+    open: true,
+  },
+  esbuild: {
+    pure: ['console.log', 'debugger'],
+  },
+  build: {
+    target: 'es2015',
+    cssTarget: 'chrome80',
+    outDir: 'dist',
+    chunkSizeWarningLimit: 2000,
+  },
+  define: {
+    __APP_INFO__: JSON.stringify(__APP_INFO__),
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        additionalData: '@root-entry-name: default;',
+      },
+    },
+    postcss: {
+      plugins: [
+        {
+          postcssPlugin: 'internal:charset-removal',
+          AtRule: {
+            charset: (atRule) => {
+              if (atRule.name === 'charset') {
+                atRule.remove();
+              }
+            },
+          },
+        },
+      ],
+    },
+  },
+  plugins: [react()],
+});
