06013e3912869221de1856f4cd35b4cf91c021d3
Nguyen Huu Trung <huutrung.mmt@gmail.com>
2022-09-29 16:12:56 +0700

    perf: optimize test

--- src/layouts/__test__/Header.test.tsx
+++ src/layouts/__test__/Header.test.tsx
@@ -5,7 +5,7 @@ import { describe, expect, test } from 'vitest';
 import { Header } from '@/layouts';
 
 describe('Header', () => {
-  test('Clean input and then focus in', async () => {
+  test('Clean input and then focus in', () => {
     render(
       <BrowserRouter>
         <Header />
@@ -14,17 +14,17 @@ describe('Header', () => {
     const inputSearch = screen.getByTestId('search') as HTMLInputElement;
     // if input has value
     fireEvent.change(inputSearch, { target: { value: 'test' } });
-    // then click icon clear to clean input and immediately remove dom icon clear
+    // then click icon clear to clean input
     const iconClear = screen.queryByTestId('clear') as HTMLElement;
     fireEvent.click(iconClear);
-    iconClear.remove();
-    // passing dom icon clear have not in document
-    expect(iconClear).not.toBeInTheDocument();
     // focus and clean input
     inputSearch.focus();
     fireEvent.change(inputSearch, { target: { value: '' } });
     // passing clean and focus input
     expect(inputSearch.value).toBe('');
     expect(inputSearch).toHaveFocus();
+    // passing dom icon clear have not in document
+    iconClear.remove();
+    expect(iconClear).not.toBeInTheDocument();
   });
 });
