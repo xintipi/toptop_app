efbaab70dff73cd40d9c310e69c5171ed365bc82
Nguyen Huu Trung <huutrung.mmt@gmail.com>
2022-09-29 15:58:50 +0700

    perf: ref souce

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
-import { describe, expect, it } from 'vitest';
-import { render, waitFor, RenderResult } from '@testing-library/react';
+import { render, RenderResult, waitFor } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { describe, expect, it } from 'vitest';
+
 import App from '@/App';
 
 describe('App', () => {
